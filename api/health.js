86c2ba1f626810d1411e8727d1cf566ed9e87ad0
emiliagrijalvao-a11y <emiliagrijalvao@gmail.com>
2025-10-03 13:32:53 -0500

    Update health.js

--- api/health.js
+++ api/health.js
@@ -1,3 +1,8 @@
-export default function handler(req, res) {
-  res.status(200).json({ ok: true });
-}
+// /api/health.js
+module.exports = async (_req, res) => {
+  res.status(200).json({
+    ok: true,
+    hasToken: !!process.env.NOTION_TOKEN,
+    hasDb: !!process.env.NOTION_DATABASE_ID,
+  });
+};
